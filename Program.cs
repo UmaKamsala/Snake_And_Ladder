8622e051fd9ab9261e8f99d0fccaec6005f0ec0a
UmaKamsala <kamsalauma@gmail.com>
2021-10-27 18:01:18 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,28 @@ namespace Snake_And_Ladder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake And Ladder Program");
+
+            UC_1 uc1 = new UC_1();
+            uc1.Game();
+
+            UC_2 dieRoll = new UC_2();
+            dieRoll.DieRoll();
+
+            UC_3 game = new UC_3();
+            game.Play();
+
+            UC_4 player = new UC_4();
+            player.Game();
+
+            UC_5 win = new UC_5();
+            win.Win();
+
+            UC_6 diecount = new UC_6();
+            diecount.DieCount();
+
+            UC_7 twoPlayers = new UC_7();
+            twoPlayers.Playing();
+
         }
     }
 }
--- UC_1.cs
+++ UC_1.cs
@@ -6,12 +6,13 @@ namespace Snake_And_Ladder
 {
     class UC_1
     {
-          public const int START = 0;
-            public void Game()
-            {
-                int playerPosition = START;
-                Console.WriteLine("Sinle Player Started At Positoin : " + playerPosition);
-            }
+        public const int START = 0;
+        public void Game()
+        {
+            int playerPosition = START;
+            Console.WriteLine("Sinle Player Started At Positoin : " + playerPosition);
+
         }
     }
+}   
 
